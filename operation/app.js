a9bbc1185cf7acf72ff339e4c698d06311369f70
alireza.Raoufi <105983557+AlirezaRaoufi84@users.noreply.github.com>
2022-06-25 19:17:50 +0430

    Add files via upload

---
+++ operation/app.js
@@ -0,0 +1,9 @@
+var first = prompt("عدد اول را وارد کنید :" , "مثال : 0")
+var firstNum = +first
+var sec = prompt("عدد دوم را وارد کنید :", "مثال : 2")
+var secNum = +sec
+
+alert("جمع اعداد برابر هست با " + (firstNum + secNum))
+alert("تفریق اعداد برابر هست با " + (firstNum - secNum))
+alert("ضرب اعداد برابر هست با " + (firstNum * secNum))
+alert("تقسیم اعداد برابر هست با " + (firstNum / secNum))
